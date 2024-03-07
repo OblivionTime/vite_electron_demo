7d8dc655ee41d4e9c1dd7f7ea096684a427a6805
Solid <xiaocaiqi_lg@163.com>
2024-03-07 14:26:16 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,7 @@
 </template>
 
 <script setup>
-import {useRouter} from 'vue-router'
-import {onMounted } from 'vue';
-const router=useRouter()
-onMounted(() => {
-  router.push("/")
-})
+
 </script>
 
 <style lang="scss" scoped></style>
